8b99c7b7f536b281df781842678ba5088042d0a3
Tr4nLa4m <tranlamiter@gmail.com>
2023-06-05 18:23:24 +0700

    upcode sidebar 90%

--- src/components/treeview/MTreeView.vue
+++ src/components/treeview/MTreeView.vue
@@ -6,15 +6,11 @@
           class="caret"
           v-if="item.children"
           :text="item.text"
-          :leftIcon="'mi-16 mi-carret-right-white'"
+          :leftIcon="getLeftIcon(item)"
           @click="toggleCollapse(item)"
         >
         </MTab>
-        <MTab 
-        v-else 
-        :text="item.text"
-        :icon="item.icon"
-        />
+        <MTab v-else :text="item.text" :icon="item.icon" />
         <MTreeView
           v-if="item.children"
           :items="item.children"
@@ -26,40 +22,90 @@
 </template>
 
 <script>
-import { defineComponent,  reactive } from "vue";
+import { defineComponent, reactive, ref , watch } from "vue";
 import MTab from "@/components/tab/TabItem.vue";
-import commonFn from "@/utils/helper/commonFn.js"
+import commonFn from "@/utils/helper/commonFn.js";
+
 export default defineComponent({
   name: "MTreeView",
-  components: {MTab},
+  components: { MTab },
   props: {
     items: {
       type: Array,
-      required: true
+      required: true,
     },
     collapsed: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+
+    searchProjectText: String,
   },
   setup(props) {
-    const data = reactive(commonFn.addCollapsedPropertyForTree(props.items));
+    const Icon = {
+      collapsed: "mi-16 mi-carret-down-white",
+      notcollased: "mi-16 mi-carret-right-white",
+    };
+
+    const currentState = ref({
+      isCollapsed: props.collapsed,
+      leftIcon: Icon.notcollased,
+    });
+
+
+    const data = ref(commonFn.addCollapsedPropertyForTree(props.items));
+
+    const updateData = () => {
+      data.value = commonFn.addCollapsedPropertyForTree(props.items);
+    };
+
+    watch(
+      () => props.items,
+      () => {
+        updateData();
+      }
+    );
+
+    // watch(
+    //   () => props.searchProjectText,
+    //   (current, old) => {
+    //     if(!current && data.value && data.value.length > 0){
+    //       debugger
+    //       data.value.forEach(element => {
+    //         if(element.children){
+    //           element.children.forEach((children) => {
+    //             children.collapsed = false;
+    //           })
+    //         }
+    //       });
+    //     }
+    //   }
+    // );
 
     const toggleCollapse = (item) => {
-      let idx = data.findIndex(x => x.id === item.id);
-      if(idx >= 0){
-        data[idx].collapsed = !item.collapsed;
+      currentState.value.leftIcon =
+        currentState.value.leftIcon == Icon.collapsed
+          ? Icon.notcollased
+          : Icon.collapsed;
+      let idx = data.value.findIndex((x) => x.id === item.id);
+      if (idx >= 0) {
+        data.value[idx].collapsed = !item.collapsed;
       }
     };
 
-    return{
+    const getLeftIcon = (item) => {
+      return item.collapsed ? Icon.notcollased : Icon.collapsed;
+    };
+
+    return {
       data,
-      toggleCollapse
-    }
-  }
-
+      currentState,
+      toggleCollapse,
+      getLeftIcon,
+      updateData
+    };
+  },
 });
-
 </script>
 
 <style scoped>
